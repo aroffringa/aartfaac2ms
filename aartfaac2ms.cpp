163255bdf0ba31e92d40aa03b2de8cece90063b6
André Offringa <offringa@gmail.com>
2018-03-17 11:48:29 +0100

    Wrote the aartfaac reader

---
+++ aartfaac2ms.cpp
@@ -0,0 +1,58 @@
+#include <complex>
+#include <iostream>
+#include <fstream>
+
+#include "uvector.h"
+
+#include "aartfaacms/aartfaacfile.h"
+
+#include <unistd.h>
+
+void allocateBuffers(const AartfaacFile& file)
+{
+	bool rfiDetection = false;
+	double memPercentage = 90.0, memLimit = 0.0;
+	long int pageCount = sysconf(_SC_PHYS_PAGES), pageSize = sysconf(_SC_PAGE_SIZE);
+	int64_t memSize = (int64_t) pageCount * (int64_t) pageSize;
+	double memSizeInGB = (double) memSize / (1024.0*1024.0*1024.0);
+	if(memLimit == 0.0)
+		std::cout << "Detected " << round(memSizeInGB*10.0)/10.0 << " GB of system memory.\n";
+	else {
+		std::cout << "Using " << round(memLimit*10.0)/10.0 << '/' << round(memSizeInGB*10.0)/10.0 << " GB of system memory.\n";
+		memSize = int64_t(memLimit * (1024.0*1024.0*1024.0));
+		memPercentage = 100.0;
+	}
+	size_t maxBufferSize = memSize*memPercentage/(100*(sizeof(float)*2+1));
+	size_t nChannels = file.NChannels();
+	size_t nAntennas = file.NAntennas();
+	size_t maxScansPerPart = maxBufferSize / (nChannels*(nAntennas+1)*nAntennas*2);
+	
+	if(maxScansPerPart<1)
+	{
+		std::cout << "WARNING! The given amount of memory is not even enough for one scan and therefore below the minimum that Cotter will need; will use more memory. Expect swapping and very poor flagging accuracy.\nWARNING! This is a *VERY BAD* condition, so better make sure to resolve it!";
+		maxScansPerPart = 1;
+	} else if(maxScansPerPart<20 && rfiDetection)
+	{
+		std::cout << "WARNING! This computer does not have enough memory for accurate flagging; expect non-optimal flagging accuracy.\n"; 
+	}
+	size_t partCount = 1 + file.NTimesteps() / maxScansPerPart;
+	if(partCount == 1)
+		std::cout << "All " << file.NTimesteps() << " scans fit in memory; no partitioning necessary.\n";
+	else
+		std::cout << "Observation does not fit fully in memory, will partition data in " << partCount << " chunks of " << (file.NTimesteps()/partCount) << " scans.\n";
+}
+
+int main(int argc, char* argv[])
+{
+	AartfaacFile file(argv[1]);
+	ao::uvector<std::complex<float>> vis(file.VisPerTimestep());
+	size_t nTimesteps = file.NTimesteps();
+	size_t index = 0;
+	allocateBuffers(file);
+	while(file.HasMore())
+	{
+		file.ReadTimestep(vis.data());
+		std::cout << index << '/' << nTimesteps << " " << vis[0] << '\n';
+		++index;
+	}
+}
---
+++ aartfaacfile.h
@@ -0,0 +1,103 @@
+#ifndef AARTFAAC_FILE_H
+#define AARTFAAC_FILE_H
+
+#include <cstdint>
+#include <sstream>
+#include <complex>
+
+struct Header {
+	static const uint32_t
+		CORR_HDR_MAGIC= 0x000000003B98F002; // Magic number in raw corr visibilities.
+	
+	uint32_t magic;
+	uint16_t nrReceivers;
+	uint8_t  nrPolarizations;
+	uint8_t  correlationMode;
+	double   startTime, endTime;
+	uint32_t weights[78]; // Fixed-sized field, independent of #stations!
+	uint32_t nrSamplesPerIntegration;
+	uint16_t nrChannels;
+
+	char     pad1[170];
+	
+	std::string ToString() {
+		std::ostringstream str;
+		str
+		<< "magic = " << magic << ' ';
+		switch(magic) {
+			case CORR_HDR_MAGIC: str << "CORR_HDR_MAGIC" << '\n'; break;
+			default: str << "????\n"; break;
+		}
+		str
+		<< "nrReceivers = " << nrReceivers << '\n'
+		<< "nrPolarizations = " << unsigned(nrPolarizations) << '\n'
+		<< "correlationMode = " << unsigned(correlationMode) << '\n'
+		<< "startTime = " << startTime << '\n'
+		<< "endTime = " << endTime << " (total: " << (endTime-startTime) << " s)\n"
+		<< "weights = [" << weights[0];
+		for(size_t i=1; i!=78; ++i)
+			str << ", " << weights[i];
+		str
+		<< "]\nnrSamplesPerIntegration = " << nrSamplesPerIntegration << '\n'
+		<< "nrChannels = " << nrChannels << '\n';
+		return str.str();
+	}
+};
+
+static_assert(sizeof(Header) == 512, "Header should be of size 512 bytes");
+
+// An AARTFAAC file has a header followed by the data, which is written as:
+// std::complex<float> visibilities[nr_baselines][nr_channels][nr_pols][nr_pols];
+class AartfaacFile {
+public:
+	AartfaacFile(const char* filename) :
+		_file(filename), _blockPos(0)
+	{
+		_file.seekg(0, std::ios::end);
+		_filesize = _file.tellg();
+		_file.seekg(0, std::ios::beg);
+		_file.read(reinterpret_cast<char*>(&_header), sizeof(Header));
+		_file.seekg(0, std::ios::beg);
+		std::cout << _header.ToString();
+		_blockSize = sizeof(std::complex<float>) * VisPerTimestep();
+		std::cout << "buffersize=" << _blockSize << " (%512=" << (_blockSize%512) << ")\n";
+		std::cout << "filesize=" << _filesize << '\n';
+		std::cout << "nblocks=" << _filesize / (_blockSize+512) << "(%=" << _filesize%(_blockSize+512) << ")\n";
+	}
+	
+	size_t VisPerTimestep() const
+	{
+		size_t nBaselines = _header.nrReceivers * (_header.nrReceivers+1) / 2;
+		return nBaselines * _header.nrChannels * _header.nrPolarizations;
+	}
+	
+	void ReadTimestep(std::complex<float>* buffer)
+	{
+		Header h;
+		_file.read(reinterpret_cast<char*>(&h), sizeof(Header));
+		//std::cout << h.ToString() << '\n';
+		_file.read(reinterpret_cast<char*>(buffer), _blockSize);
+		if(!_file)
+			throw std::runtime_error("Error reading file");
+		++_blockPos;
+	}
+	
+	bool HasMore() const
+	{
+		return _blockPos < (_filesize / _blockSize);
+	}
+	
+	size_t NTimesteps() const
+	{
+		return _filesize / _blockSize;
+	}
+	
+	size_t NChannels() const { return _header.nrChannels; }
+	size_t NAntennas() const { return _header.nrReceivers; }
+private:
+	std::ifstream _file;
+	Header _header;
+	size_t _blockSize, _filesize, _blockPos;
+};
+
+#endif
